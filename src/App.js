e424b629c4e7ef57c45f2f5fe97bcecef27f7bc0
Bektur Muratov <muratov_b@au.kg>
2024-12-05 10:37:15 +0600

    Added variants and sizes to btns

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import { Button } from "./components/Button"
 function App() {
   return (
     <div>
-      <Button>
-        Add
-      </Button>
+      <Button variant="primary" size="sm">Add</Button>
+      <Button variant="primary">Add</Button>
+      <Button variant="primary" size="lg">Add</Button>
+      <Button variant="bordered" size="sm">Cancel</Button>
+      <Button variant="bordered">Cancel</Button>
+      <Button variant="bordered" size="lg">Cancel</Button>
     </div>
   );
 }
--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,6 +1,10 @@
 import "./index.css"
 
 export function Button(props) {
-    const {variant = "primary", children} = props;
-    return <button className = "btn">{children}</button>;
+    const {variant = "primary", size="md", children} = props;
+    const className = `btn btn--${variant} btn--${size}`;
+
+    return <button className = {className}>{children}</button>;
 }
+
+export default Button;
