73c374d876ccc1729ab960acde09c8dcb02a0a57
MuratovB <muratov_b@auca.kg>
2024-12-12 18:30:49 +0600

    Finished Final project

--- src/App_final.js
+++ src/App_final.js
@@ -10,14 +10,17 @@ function App_final() {
     const [cryptocurrencies, setCryptocurrencies] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    const [searchQuery, setSearchQuery] = useState("");
 
-    useEffect(() => {
+    const fetchData = () => {
+        setLoading(true);
+        setError(null);
         fetch('https://api.coinlore.net/api/tickers/')
         .then((response) => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
             }
-        return response.json();
+            return response.json();
         })
         .then((data) => {
             setCryptocurrencies(data.data);
@@ -27,33 +30,83 @@ function App_final() {
             setError(error);
             setLoading(false);
         });
+    };
+
+    useEffect(() => {
+        fetchData();
     }, []);
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+    const handleUpdateClick = (event) => {
+        event.preventDefault(); // Останавливаем стандартное поведение
+        fetchData();
+    };
 
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  }
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    }
+
+    const filteredCryptocurrencies = cryptocurrencies.filter((crypto) => {
+        const lowerCaseQuery = searchQuery.toLowerCase();
+        return (
+            crypto.name.toLowerCase().includes(lowerCaseQuery) ||
+            crypto.symbol.toLowerCase().includes(lowerCaseQuery)
+        );
+    });
 
     return (
         <div id="appFinalRoot">
             <Heading level="1">Cryptocurrency Prices</Heading>
-            <Button variant="bordered" onClick="Update">Update</Button>
-            <Input placeholder="Search"></Input>
-
-            {cryptocurrencies.map((crypto) => (
-            <Accordion
-                key={crypto.id}
-                name={crypto.name}
-                symbol={crypto.symbol}
-                price={crypto.price_usd}
-                percentChange24h={crypto.percent_change_24h}
+            <form onSubmit={(e) => e.preventDefault()}> {/* Здесь предотвращаем отправку формы */}
+                <Button type="button" variant="bordered" onClick={handleUpdateClick}>Update</Button>
+            </form>
+            <Input 
+                placeholder="Search" 
+                value={searchQuery} 
+                onChange={(e) => setSearchQuery(e.target.value)}
             />
-      ))}
+
+            {filteredCryptocurrencies.map((crypto) => {
+                const percentChangeClass = parseFloat(crypto.percent_change_24h) < 0 ? 'negative' : 'positive';
+
+                return (
+                    <Accordion key={crypto.id} title={crypto.name}>
+                        <Typography textSize="md">
+                            <Tooltip text="The symbol of the cryptocurrency" position="top">
+                                <b>Symbol: </b>{crypto.symbol}
+                            </Tooltip>
+                        </Typography>
+                        <Typography textSize="md">
+                            <Tooltip text="The price in USD" position="top">
+                                <b>Price USD: </b>{crypto.price_usd}
+                            </Tooltip>
+                        </Typography>
+                        <Typography textSize="md">
+                            <Tooltip text="The price in BTC" position="top">
+                                <b>Price BTC: </b>{crypto.price_btc}
+                            </Tooltip>
+                        </Typography>
+                        <Typography textSize="md">
+                            <Tooltip text="The market capitalization of a cryptocurrency is calculated by multiplying the number of coins in circulation by the current price" position="top">
+                                <b>Market Cap USD: </b>{crypto.market_cap_usd}
+                            </Tooltip>
+                        </Typography>
+                        <Typography textSize="md">
+                            <Tooltip text="The percentage change in the last 24 hours" position="top">
+                                <b>Percent Change 24H: </b>
+                                <span className={percentChangeClass}>
+                                    {crypto.percent_change_24h}%
+                                </span>
+                            </Tooltip>
+                        </Typography>
+                    </Accordion>
+                );
+            })}
         </div>
-    )
+    );
 }
 
 export default App_final;
--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -2,7 +2,7 @@ import "./index.css"
 import React from 'react';
 
 export function Button(props) {
-    const {type="button", onClick="doNothing", disabled=false, variant="primary", size="md", children} = props;
+    const {type="button", onClick, disabled=false, variant="primary", size="md", children} = props;
     const className = `btn btn--${variant} btn--${size}`;
 
     const doNothing = () => {
@@ -14,6 +14,13 @@ export function Button(props) {
     switch (onClick) {
         case "doNothing":
             onClickHandler = doNothing;
+            break;
+        default:
+            if (typeof onClick !== "string"){
+                onClickHandler = onClick;
+            } else {
+                onClickHandler = doNothing;
+            }
             break;
     }
 
